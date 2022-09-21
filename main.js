0140b5a9bced9cb23a5f1e5053f8cfdebcea3e76
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-21 11:50:36 +0100

    ajouter les solution pour stocker localement un objet json.string(objet) : convertion objet à string json.parse(string) : concertion string à objet !!attention on peut pas stocker dans le localStorage sauf les strings il faut tout convertir au string

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 //localStorage.setItem("clé","valeur")
 // localStorage.getItem("clé")
 // localStorage.clear()
+//json.stringify(objet)
+//json.parse(string)
 
-
-if (localStorage.getItem("nom") != null) {
-    h1.textContent = `Bonjour ${localStorage.getItem("nom")}`;
+const local = JSON.parse(localStorage.getItem("user")); 
+if (local != null) {
+    h1.textContent = `Bonjour ${local.nom}, votre age est ${local.age}`;
 }
 
 bouton.onclick = () => {
-    localStorage.setItem("nom", nom.value);
-
+    const user = {
+        nom : nom.value,
+        age : age.value
+    }
+    localStorage.setItem("user",JSON.stringify(user));
+    document.location.reload();
 }
 clear.onclick = () => {
     localStorage.clear()
